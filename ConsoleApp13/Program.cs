896aaad98d0cd8e54cbd9ff7a1c00c67bbed9561
Kommandor <a.tsymbaliuk2007@gmail.com>
2025-02-18 18:23:18 +0200

    add task1

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -10,6 +10,22 @@ namespace ConsoleApp13
     {
         static void Main(string[] args)
         {
+
+            Func<int, bool> isEven = delegate (int number)
+            {
+                return number % 2 == 0;
+            };
+
+            Console.WriteLine("Тестування парності чисел:");
+
+            int[] testNumbers = { 10, 15, 22, 33, 42 };
+
+            foreach (var number in testNumbers)
+            {
+                Console.WriteLine($"Число {number} {(isEven(number) ? "парне" : "непарне")}");
+            }
         }
     }
 }
+    
+
