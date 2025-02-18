0784cb70e03040c98ba7616e050cee3bdc9f17c7
Kommandor <a.tsymbaliuk2007@gmail.com>
2025-02-18 18:42:33 +0200

    add task3

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -6,93 +6,14 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp13
 {
-  
-        internal class Program
+
+    internal class Program
+    {
+        static void Main()
         {
-            public class Item
-            {
-                public string Name { get; set; }
-                public double Volume { get; set; }
-
-                public Item(string name, double volume)
-                {
-                    Name = name;
-                    Volume = volume;
-                }
-            }
-            public class Backpack
-            {
-                public string Color { get; set; }
-                public string Brand { get; set; }
-                public string Fabric { get; set; }
-                public double Weight { get; set; }
-                public double Volume { get; set; }
-                public double CurrentVolume { get; private set; }
-                public List<Item> Contents { get; set; }
-                public event EventHandler<Item> ItemAdded;
-                public Backpack(string color, string brand, string fabric, double weight, double volume)
-                {
-                    Color = color;
-                    Brand = brand;
-                    Fabric = fabric;
-                    Weight = weight;
-                    Volume = volume;
-                    CurrentVolume = 0;
-                    Contents = new List<Item>();
-                }
-                public void AddItem(Item item)
-                {
-                    if (CurrentVolume + item.Volume > Volume)
-                    {
-                        throw new InvalidOperationException("Не вистачає місця в рюкзаку для цього предмета!");
-                    }
-
-                    Contents.Add(item);
-                    CurrentVolume += item.Volume;
-                    ItemAdded?.Invoke(this, item);
-                }
-                public void SetBackpackDetails(string color, string brand, string fabric, double weight, double volume)
-                {
-                    Color = color;
-                    Brand = brand;
-                    Fabric = fabric;
-                    Weight = weight;
-                    Volume = volume;
-                    CurrentVolume = 0;
-                }
-            }
-
-            static void Main()
-            {
-
-                Backpack myBackpack = new Backpack("Червоний", "Nike", "Нейлон", 1.5, 20.0);
-                myBackpack.ItemAdded += delegate (object sender, Item e)
-                {
-                    Console.WriteLine($"Предмет '{e.Name}' додано до рюкзака.");
-                };
-                Item laptop = new Item("Ноутбук", 5.0);
-                Item bottle = new Item("Пляшка", 1.5);
-                Item book = new Item("Книга", 2.0);
-
-                try
-                {
-
-                    myBackpack.AddItem(laptop);
-                    myBackpack.AddItem(bottle);
-                    myBackpack.AddItem(book);
-                    Item largeItem = new Item("Великий предмет", 15.0);
-                    myBackpack.AddItem(largeItem);
-                }
-                catch (InvalidOperationException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
+            Func<int[], int> countMultiplesOfSeven = arr => arr.Count(x => x % 7 == 0);
+            int[] numbers = { 7, 14, 21, 9, 28, 35, 3, 18 };
+            Console.WriteLine("Кількість чисел, кратних 7: " + countMultiplesOfSeven(numbers));
         }
     }
-
-
-
-
-
-    
+}
