0dff4acf0c7a48efd4c97cb3b9a754d96e53aed3
Kommandor <a.tsymbaliuk2007@gmail.com>
2025-02-18 18:46:36 +0200

    add task5

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -11,10 +11,11 @@ namespace ConsoleApp13
     {
         static void Main()
         {
-
-            Func<int[], int> countPositiveNumbers = arr => arr.Count(x => x > 0);
-            int[] numbers = { 7, -2, 14, 0, -3, 18, 5 };
-            Console.WriteLine("Кількість позитивних чисел: " + countPositiveNumbers(numbers));
+            Func<string, string, bool> containsWord = (text, word) => text.Contains(word);
+            string text = "Це простий тестовий текст для перевірки.";
+            string wordToFind = "тестовий";
+            bool result = containsWord(text, wordToFind);
+            Console.WriteLine($"Текст містить слово '{wordToFind}': {result}");
         }
     }
 }
