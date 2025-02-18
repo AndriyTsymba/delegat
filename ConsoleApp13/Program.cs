cc2d549336ebdef0b0ae653670fc557a78e7b59c
Kommandor <a.tsymbaliuk2007@gmail.com>
2025-02-18 18:28:51 +0200

    add task4

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -10,21 +10,25 @@ namespace ConsoleApp13
     {
         static void Main(string[] args)
         {
-
-            Func<int, int> cube = x => x * x * x;
-
-
-            Console.WriteLine("Тестування підрахунку куба чисел:");
-
-            int[] testNumbers = { 2, 3, 4, 5, 6 };
-
-            foreach (var number in testNumbers)
+            Func<DateTime, bool> isProgrammersDay = date =>
             {
-                Console.WriteLine($"Куб числа {number} = {cube(number)}");
+                int year = date.Year;
+                DateTime programmersDay = new DateTime(year, 1, 1).AddDays(255);
+
+                return date.Date == programmersDay.Date;
+            };
+            Console.WriteLine("Тестування дня програміста:");
+            DateTime[] testDates = {
+            new DateTime(2025, 9, 13), 
+            new DateTime(2025, 9, 14), 
+            new DateTime(2024, 9, 12), 
+            new DateTime(2023, 9, 13)
+            };
+            foreach (var date in testDates)
+            {
+                Console.WriteLine($"{date.ToString("yyyy-MM-dd")} є днем програміста: {isProgrammersDay(date)}");
             }
         }
     }
 }
-
-
     
