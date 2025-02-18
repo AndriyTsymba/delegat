73707c1c8af5dc6ecd15b547877375b909e5d55a
Kommandor <a.tsymbaliuk2007@gmail.com>
2025-02-18 18:36:03 +0200

    add task1

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -10,11 +10,39 @@ namespace ConsoleApp13
     {
         static void Main(string[] args)
         {
-            Func<int[], int> findMax = arr => arr.Min();
-            int[] numbers = { 12, 5, 8, 20, 6, 15 };
-            Console.WriteLine("Мінімум в масиві: " + findMax(numbers));
+            Func<string, (int, int, int)> getRgbValue = delegate (string color)
+            {
+                switch (color.ToLower())
+                {
+                    case "red":
+                        return (255, 0, 0);  
+                    case "orange":
+                        return (255, 165, 0); 
+                    case "yellow":
+                        return (255, 255, 0); 
+                    case "green":
+                        return (0, 255, 0);  
+                    case "blue":
+                        return (0, 0, 255);   
+                    case "indigo":
+                        return (75, 0, 130);  
+                    case "violet":
+                        return (238, 130, 238); 
+                    default:
+                        return (0, 0, 0);   
+                }
+            };
+            Console.WriteLine("Тестування RGB значень для кольорів веселки:");
+
+            string[] rainbowColors = { "Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet" };
+
+            foreach (var color in rainbowColors)
+            {
+                var (r, g, b) = getRgbValue(color);
+                Console.WriteLine($"RGB для {color}: ({r}, {g}, {b})");
+            }
         }
     }
 }
-  
+
     
